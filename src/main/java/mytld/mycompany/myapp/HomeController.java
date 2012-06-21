6c2531814b72b46e6861524fab82415e2e5f554b
Kaitlin Duck Sherwood <kate.sherwood@tasktop.com>
2012-06-21 09:52:32 -0700

    Ooops, typo; rolled rev #

--- src/main/java/mytld/mycompany/myapp/HomeController.java
+++ src/main/java/mytld/mycompany/myapp/HomeController.java
@@ -32,7 +32,7 @@ public class HomeController {
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate );
-		model.addAttribute("local", locale.toString() );
+		model.addAttribute("locale", locale.toString() );
 		
 		return "home";
 	}
